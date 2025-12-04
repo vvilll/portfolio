7010bc97499976ab7c894f7c9d777910451e43ae
William <wtwotherspoon@nic.edu>
2025-12-04 00:04:09 -0800

    added comments cleaned up code

--- JS/buttonScript.js
+++ JS/buttonScript.js
@@ -1,31 +1,35 @@
 function rotateImage(rotI){
     const images = ["images/huntingSeasonProj.jpg","images/assemblyProj.jpg","images/stegoProj.jpg"];
+    var imgElem = document.getElementById("projImage")
+    
     if(rotI == 1){
-        if(document.getElementById("projImage").src.includes(images[0])){
-            document.getElementById("projImage").src = images[1];
+        //check for current elem
+        if(imgElem.src.includes(images[0])){
+            //change img to next and update buttons
+            imgElem.src = images[1];
             document.getElementsByClassName("controlButton left")[0].style.backgroundColor = '#e7e7e7';
             document.getElementsByClassName("controlButton center")[0].style.backgroundColor = '#252525';
-        }else if(document.getElementById("projImage").src.includes(images[1])){
-            document.getElementById("projImage").src = images[2];
+        }else if(imgElem.src.includes(images[1])){
+            imgElem.src = images[2];
             document.getElementsByClassName("controlButton center")[0].style.backgroundColor = '#e7e7e7';
             document.getElementsByClassName("controlButton right")[0].style.backgroundColor = '#252525';
         }else{
-            document.getElementById("projImage").src = images[0];
+            imgElem.src = images[0];
             document.getElementsByClassName("controlButton right")[0].style.backgroundColor = '#e7e7e7';
             document.getElementsByClassName("controlButton left")[0].style.backgroundColor = '#252525';
         }
     }
     if(rotI == 0){
-        if(document.getElementById("projImage").src.includes(images[0])){
-            document.getElementById("projImage").src = images[2];
+        if(imgElem.src.includes(images[0])){
+            imgElem.src = images[2];
             document.getElementsByClassName("controlButton left")[0].style.backgroundColor = '#e7e7e7';
             document.getElementsByClassName("controlButton right")[0].style.backgroundColor = '#252525';
-        }else if(document.getElementById("projImage").src.includes(images[1])){
-            document.getElementById("projImage").src = images[0];
+        }else if(imgElem.src.includes(images[1])){
+            imgElem.src = images[0];
             document.getElementsByClassName("controlButton center")[0].style.backgroundColor = '#e7e7e7';
             document.getElementsByClassName("controlButton left")[0].style.backgroundColor = '#252525';
         }else{
-            document.getElementById("projImage").src = images[1];
+            imgElem.src = images[1];
             document.getElementsByClassName("controlButton right")[0].style.backgroundColor = '#e7e7e7';
             document.getElementsByClassName("controlButton center")[0].style.backgroundColor = '#252525';
         }
@@ -34,6 +38,7 @@ function rotateImage(rotI){
 
 function changeImg(chng){
     if(chng == 0){
+        //move to next img and set color for buttons
         document.getElementById("projImage").src = "images/huntingSeasonProj.jpg";
         document.getElementsByClassName("controlButton center")[0].style.backgroundColor = '#e7e7e7';
         document.getElementsByClassName("controlButton right")[0].style.backgroundColor = '#e7e7e7';
